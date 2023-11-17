019c142f88d72ae55de9aa3d9df9b081ee902897
GKAYDER724 <gkaydertk@gmail.com>
2023-11-17 17:26:04 +0700

    [HTML]- Quản lý loại sản phẩm

---
+++ admin/pages/category/edit.php
@@ -0,0 +1,16 @@
+<div class="bg-secondary rounded h-100 p-4">
+    <h6 class="mb-4">THÊM LOẠI SẢN PHẨM</h6>
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Loại sản phẩm:</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">
+            </div>
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Thêm loại</button>
+    </form>
+</div>
