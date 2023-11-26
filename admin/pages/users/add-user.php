047d9103c54772adda3f133471d76b64f52ed369
NguyenHoangDuyKhang <khang12a3lqd@gmail.com>
2023-11-26 10:42:12 +0700

    [EK06] - Cập nhât validate form - Nguyễn Hoàng Duy Khang

--- admin/pages/users/add-user.php
+++ admin/pages/users/add-user.php
@@ -1,14 +1,62 @@
 <?php
     include_once 'pages/users/user-function.php';
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $user = $_POST['user'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $sex = $_POST['sex'];
-    $role = $_POST['role'];
-    $data = add_user($user, $email, $password,$sex, $role);
-}
+    // Tạo biến bắt validate
+//    $user = $email = $password = $sex = $role = "";
+    $user_error = $email_error = $password_error = $sex_error = $role_error = "";
+    $check = false;
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["user"])) {
+        $check = true;
+        $user_error = "<span style='color:red;'>Error: Họ tên bắt buộc phải nhập.</span>";
+    } else{
+        $user = $_POST['user'];
+    }
+
+    if(empty($_POST["email"])){
+        $email_error = "<span style='color:red;'>Error: Email bắt buộc phải nhập.</span>";
+        $check == true;
+    } else{
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $email_error = "<span style='color:red;'>Error: Email nhập chưa đúng.</span>";
+            $check == true;
+        } else if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+        $email = $_POST['email'];
+        $connect = new connect();
+        $data = "SELECT email FROM users WHERE email = '$email'";
+        $check_email = $connect->pdo_query_one($data);
+            if (!empty($check_email)){
+                $email_check_error = "<span style='color:red;'>Error: Email đã được đăng ký tài khoảng, vui lòng nhập email khác!</span>";
+                $check = true;
+            }
+        }
+    }
+
+    if(empty($_POST["password"])){
+        $check = true;
+        $password_error = "<span style='color:red;'>Error: Mật khẩu bắt buộc phải nhập.</span>";
+    } else{
+        $password = $_POST['password'];
+    }
+
+    if (empty($_POST["sex"])){
+        $check = true;
+        $sex_error = "<span style='color:red;'>Error: Giới tính bắt buộc phải chọn.</span>";
+    } else{
+        $sex = $_POST['sex'];
+    }
+
+    if (empty($_POST['role'])){
+        $check = true;
+        $role_error = "<span style='color:red;'>Error: Chức vụ bắt buộc phải chọn.</span>";
+    } else{
+        $role = $_POST['role'];
+    }
+
+    if ($check == false){
+        $data = add_user($user, $email, $password,$sex, $role);
+    }
+}
 ?>
 
 <div class="col-3"></div>
@@ -17,18 +65,37 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <div class="bg-secondary rounded h-100 p-4">
         <h6 class="mb-4">THÊM NGƯỜI DÙNG</h6>
         <div class="form-floating mb-3">
-            <input type="text" name="user" class="form-control" id="floatingInput" placeholder="" required >
+            <input type="text" name="user" class="form-control" id="floatingInput" placeholder=""  value="">
             <label for="floatingInput">Tên Người Dùng</label>
         </div>
+        <?php
+        if (isset($user_error)){
+            echo $user_error;
+        }
+        ?>
+
         <div class="form-floating mb-3">
-            <input type="email" name="email" class="form-control" id="floatingInput" placeholder="" required >
+            <input type="email" name="email" class="form-control" id="floatingInput" placeholder=""  value="">
             <label for="floatingInput">Email Người Dùng</label>
         </div>
+        <?php
+        if (isset($email_error)){
+            echo $email_error;
+        }
+        if (isset($email_check_error)){
+            echo $email_check_error;
+        }
+        ?>
+
         <div class="form-floating mb-3">
-            <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" required>
+            <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password"  value="">
             <label for="floatingPassword">Password</label>
         </div>
-
+        <?php
+        if (isset($password_error)){
+            echo $password_error;
+        }
+        ?>
         <div class="form-floating mb-3">
             <label for="exampleInputEmail1">Giới Tính</label><br/><br/>
             <input type="radio" name="sex" id="" value="male"> Nam
@@ -37,22 +104,32 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             <br/>
             <input type="radio" name="sex" id="" value="orther"> Khác
         </div>
+        <?php
+        if (isset($sex_error)){
+            echo $sex_error;
+        }
+        ?>
         <div class="form-floating mb-3">
             <label for="exampleInputEmail1">Chức Vụ</label><br/><br/>
             <input type="radio" name="role" id="" value="1"> Admin
             <br/>
-            <input type="radio" name="role" id="" value="0"> Người Dùng
+            <input type="radio" name="role" id="" value="2"> Creators
+            <br/>
+            <input type="radio" name="role" id="" value="3"> Product Manager
+            <br/>
+            <input type="radio" name="role" id="" value="0"> User
         </div>
+        <?php
+        if (isset($role_error)){
+            echo $role_error;
+        }
+        ?>
         <br/>
         <button type="submit" class="btn btn-outline-primary">Thêm</button>
     </form>
 
     <div>
-        <?php
-        if (isset($thongbao)){
-            echo $thongbao;
-        }
-        ?>
+
     </div>
 
     </div>
