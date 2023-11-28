7abf4b3ca8ef4418938d14b2fcf0d442e3802b6d
ngohuy77 <144972341+ngohuy77@users.noreply.github.com>
2023-11-28 14:27:43 +0700

    [EK9.3] - Sửa icon edit và delete list của quản lý sản phẩm

--- admin/pages/products/search.php
+++ admin/pages/products/search.php
@@ -45,13 +45,13 @@ if (isset($_POST['search'])) {
                 </td>
                 <td>
                     <?php if ($pro->CountVariants($product['product_id']) > 0): ?>
-                        <a class="btn btn-sm btn-primary"
-                           href="?page=product&action=edit-variants&product=<?= $product['product_id'] ?>">Chỉnh
-                            sửa</a>
+                        <a class="btn btn-sm btn-warning"
+                           href="?page=product&action=edit-variants&product=<?= $product['product_id'] ?>"><i
+                                    class="fa-sharp fa-solid fa-pen-to-square"></i></a>
                         <button type="button" data-bs-toggle="modal"
                                 data-bs-target="#Delete<?= $product['product_id'] ?>"
                                 class="btn btn-sm  btn-primary">
-                            Xóa
+                            <i class="fa-solid fa-trash"></i>
                         </button>
                         <div class="modal fade" id="Delete<?= $product['product_id'] ?>" tabindex="-1"
                              aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -82,12 +82,13 @@ if (isset($_POST['search'])) {
                             </div>
                         </div>
                     <?php else: ?>
-                        <a class="btn btn-sm btn-primary"
-                           href="?page=product&action=edit&product=<?= $product['product_id'] ?>">Chỉnh sửa</a>
+                        <a class="btn btn-sm btn-warning"
+                           href="?page=product&action=edit&product=<?= $product['product_id'] ?>"><i
+                                    class="fa-sharp fa-solid fa-pen-to-square"></i></a>
                         <button type="button" data-bs-toggle="modal"
                                 data-bs-target="#Delete<?= $product['product_id'] ?>"
                                 class="btn btn-sm  btn-primary">
-                            Xóa
+                            <i class="fa-solid fa-trash"></i>
                         </button>
                         <div class="modal fade" id="Delete<?= $product['product_id'] ?>" tabindex="-1"
                              aria-labelledby="exampleModalLabel" aria-hidden="true">
