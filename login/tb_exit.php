c86d7c7503a909409368cb0d1e7fb4d7644c51c5
NguyenHoangDuyKhang <khang12a3lqd@gmail.com>
2023-12-05 15:33:33 +0700

    [EK17] - Quên mật khẩu người dùng - Nguyễn Hoàng Duy Khang

---
+++ login/tb_exit.php
@@ -0,0 +1,9 @@
+<div class="row   rounded align-items-center justify-content-center mx-0">
+    <div class="col-md-6 text-center p-4">
+        <h1 class="mb-4" style="font-family: Roboto; color: red">Đường dẫn đã hết hạng sử dụng!</h1>
+        <p class="mb-4" style="color: #0c0e12; font-family: Roboto; font-size: 20px">Vui lòng yêu cầu cấp lại mã OTP
+            hoặc liên hệ trực tiếp đến công ty Molla</p>
+        <a class="btn btn-primary rounded-pill py-3 px-5" href="?action=home" style="font-family: Roboto">Về trang
+            chủ</a>
+    </div>
+</div>
